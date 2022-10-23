ffa810d4cf28d2aadf07000be9c6bb33bbccb6a0
noahg09 <noah.gruenig@hispeed.ch>
2022-10-23 13:27:41 +0200

    first Commit

---
+++ src/main/java/ch/bbw/ng/testtest/TestTestApplication.java
@@ -0,0 +1,13 @@
+package ch.bbw.ng.testtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestTestApplication.class, args);
+    }
+
+}
